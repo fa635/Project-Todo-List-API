0af5bf4e4df701e1f39fce8135b6089e9e563462
fa <bfalonne823@gmail.com>
2025-04-13 11:02:24 +0200

    Base skeleton

---
+++ src/main/java/com/fa/projecttodolistapi/ProjecttodolistapiApplication.java
@@ -0,0 +1,13 @@
+package com.fa.projecttodolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttodolistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttodolistapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fa/projecttodolistapi/ProjecttodolistapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fa.projecttodolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttodolistapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
