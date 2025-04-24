2c007d74339d8db8bf251d1679a43fe1be7cd06b
fa <bfalonne823@gmail.com>
2025-04-24 10:16:41 +0200

    >Test createTodo

--- src/test/java/com/fa/projecttodolistapi/controller/TodoControllerTest.java
+++ src/test/java/com/fa/projecttodolistapi/controller/TodoControllerTest.java
@@ -19,22 +19,24 @@ public class TodoControllerTest {
 
     private TodoRepository todoRepository;
     private TodoController todoController;
+    private Todo todo1;
+    private Todo todo2;
 
     @BeforeEach
     public void setUp() {
         todoRepository = mock(TodoRepository.class);
         todoController = new TodoController(todoRepository);
+
+        todo1 = new Todo();
+        Todo todo2 = new Todo();
+        todo1.setTitle("Learn JUnit");
+        todo2.setTitle("Write tests");
+        todo1.setTodoPriority(TodoPriority.HIGH);
+        todo2.setTodoPriority(TodoPriority.MEDIUM);
     }
 
     @Test
     public void testGetAllTodos_ReturnsTodos() {
-        Todo todo1 = new Todo();
-        todo1.setTitle("Learn JUnit");
-        todo1.setTodoPriority(TodoPriority.HIGH);
-
-        Todo todo2 = new Todo();
-        todo2.setTitle("Write tests");
-        todo2.setTodoPriority(TodoPriority.MEDIUM);
 
         List<Todo> fakeTodos = Arrays.asList(todo1, todo2);
 
@@ -45,6 +47,22 @@ public class TodoControllerTest {
         assertNotNull(result);
         assertEquals(fakeTodos, result);
     }
+
+
+    @Test
+    public void testCreateTodo_CreatesTodoSuccessfully() {
+
+        when(todoRepository.save(todo1)).thenReturn(todo1);
+
+        Todo createdTodo = todoController.createTodo(todo1);
+
+        assertNotNull(createdTodo);
+        assertEquals(todo1.getTitle(), createdTodo.getTitle());
+        assertEquals(todo1.getTodoPriority(), createdTodo.getTodoPriority());
+        
+        verify(todoRepository, times(1)).save(todo1);
+    }
+
 
 }
 
